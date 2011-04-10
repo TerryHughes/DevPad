e1d51e486ba6c278c787a1dfe9e4c3facbf1215b
TerryH <terry@tnthughes.net>
2011-04-09 18:09:10 -0700

    Update the build script to make it easier to run the newest code

--- make.cmd
+++ make.cmd
@@ -1,8 +1,15 @@
 @ECHO OFF
 
+IF EXIST obj RMDIR obj /S /Q
+MKDIR obj
+
+"%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe" /out:obj\DevPad.exe /target:winexe /recurse:src\*.cs /nologo
+
+IF ERRORLEVEL 1 GOTO :EoF
+
 IF EXIST bin RMDIR bin /S /Q
 MKDIR bin
 
-"%windir%\Microsoft.NET\Framework\v4.0.30319\csc.exe" /out:bin\DevPad.exe /target:winexe /recurse:src\*.cs /nologo
+COPY obj\*.* bin >> NUL
 
 @ECHO ON
---
+++ upgrayedd.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+CALL make.cmd
+@ECHO OFF
+START bin\DevPad.exe
+
+@ECHO ON
