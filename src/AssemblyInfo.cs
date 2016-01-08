a4f52a25fa537b95717d8a845cf51bdee04ccfca
TerryH <terry@tnthughes.net>
2016-01-07 17:23:15 -0800

    Update the copyright year

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("TerryH")]
 [assembly: AssemblyProduct("DevPad")]
-[assembly: AssemblyCopyright("Copyright © TerryH 2011")]
+[assembly: AssemblyCopyright("Copyright © TerryH 2011-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
